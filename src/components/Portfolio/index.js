89d97abbd63fa6b037f4de75a5532635fb570b74
Chris Dowd <chrisdowd1996@gmail.com>
2022-06-29 21:45:54 -0500

    first commit

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
+import { portfolio } from "../../data/portfolio";
+
+const Portfolio = () => { 
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }); 
+    
+    return (
+        <>
+            <div className="container portfolio-page">
+                <h1 className="page-title">
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={"Portfolio".split("")}
+                        idx={15}
+                    />
+                </h1>
+                <div className="images-container">
+                    {portfolio.map(project => (
+                        <div className="image-box" key={project.id}>
+                                <img src={project.image} className="portfolio-image" alt="portfolio" />
+                            <div className="content">
+                                <p className="title">{project.title}</p>
+                                <h4 className="description">{project.description}</h4>
+                                <button className="btn" onClick={() => window.open(project.url)}>
+                                    View
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <Loader type="pacman" />
+        </>
+    );
+}
+
+export default Portfolio;
---
+++ src/data/portfolio.js
@@ -0,0 +1,50 @@
+import cater from '../assets/images/cater.jpg';
+import movieTheater from '../assets/images/movie-theater.jpg';
+import restaurant from '../assets/images/restaurant.jpg';
+import students from '../assets/images/students.jpg';
+import timer from '../assets/images/timer.jpg';
+import love from '../assets/images/love.png';
+export const portfolio = [
+    {
+        "id": 1,
+        "image": restaurant,
+        "title": "Restaurant Reservation",
+        "description": "React, JSX, Bootstrap, Express.js",
+        "url": "http://google.com"
+    },
+    {
+        "id": 2,
+        "image": movieTheater,
+        "title": "WeLoveMovies",
+        "description": "React, JSX, Bootstrap, Express.js",
+        "url": "http://google.com"
+    },
+    {
+        "id": 3,
+        "image": cater,
+        "title": "GrubDash",
+        "description": "Express.js",
+        "url": "http://google.com"
+    },
+    {
+        "id": 4,
+        "image": students,
+        "title": "Flashcard",
+        "description": "React, JSX",
+        "url": "http://google.com"
+    },
+    {
+        "id": 5,
+        "image": timer,
+        "title": "Pomodoro Timer",
+        "description": "React, JSX",
+        "url": "http://google.com"
+    },
+    {
+        "id": 6,
+        "image": love,
+        "title": "Thinkful Cupid",
+        "description": "HTML, CSS",
+        "url": "http://google.com"
+    }
+]
