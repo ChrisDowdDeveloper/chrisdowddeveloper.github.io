168b4b884648d07aa29796a3a86c4fdae0ddf6e2
Chris Dowd <91644551+ChrisDowdDeveloper@users.noreply.github.com>
2022-07-16 21:25:23 -0500

    public key add

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,6 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Loader from 'react-loaders'
 import { MapContainer, TileLayer } from 'react-leaflet'
-import { useRef } from 'react'
 import emailjs from '@emailjs/browser'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
@@ -23,13 +22,7 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault()
 
-    emailjs
-      .sendForm(
-        'service_3zf9ogi',
-        'template_djetb8o',
-        form.current,
-        'UBPaWUnmCOtwJYEXB'
-      )
+    emailjs.sendForm('service_3zf9ogi', 'template_djetb8o', form.current, 'UBPaWUnmCOtwJYEXB')
       .then(
         () => {
           alert('Message successfully sent!')
